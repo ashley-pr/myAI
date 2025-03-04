8e5cb2670ed94c7e923cda646179921cbece553b
Ashley Price <97683475+ashley-pr@users.noreply.github.com>
2025-03-04 15:23:41 -0500

    Update ui.ts
    Change clear convo button label

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -3,7 +3,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 export const CHAT_HEADER: string = `UNC Study Abroad Assistant`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Ask me about study abroad opportunities available through UNC`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
+export const CLEAR_BUTTON_TEXT: string = `Clear conversation`;
 export const PAGE_TITLE: string = `UNC Study Abroad Assistant`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 

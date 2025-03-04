b058c38a2f249f74caef12fc50b0bbe05f630774
Ashley Price <97683475+ashley-pr@users.noreply.github.com>
2025-03-04 14:56:43 -0500

    Update identity.ts
    Change AI_name and Owner_name

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `UNC Study Abroad`;
+export const OWNER_DESCRIPTION: string = `University of North Carolina's Study Abroad Department`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Abroad Assistant`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;

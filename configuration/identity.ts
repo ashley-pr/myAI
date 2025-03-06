30939ccefa65de8239a4802806f8b086f8b17d51
Ashley Price <97683475+ashley-pr@users.noreply.github.com>
2025-03-06 11:20:51 -0500

    Update identity.ts
    Added a human name

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `UNC Study Abroad`;
 export const OWNER_DESCRIPTION: string = `University of North Carolina's Study Abroad Department`;
 
-export const AI_NAME: string = `Abroad Assistant`;
+export const AI_NAME: string = `Sora`;
 export const AI_TONE: string = `Helpful`;
 export const AI_ROLE: string = `Study Abroad Advisor`;
